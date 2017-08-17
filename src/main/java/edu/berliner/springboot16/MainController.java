628dd911951d0ae53524d772cc724dcfbc12e7ca
Jesse <jesseberliner@gmail.com>
2017-08-17 09:31:40 -0400

    Initial commit

---
+++ src/main/java/edu/berliner/springboot16/MainController.java
@@ -0,0 +1,27 @@
+package edu.berliner.springboot16;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController
+{
+
+    @RequestMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+
+    @RequestMapping("/2")
+    public String page2()
+    {
+        return "page2";
+    }
+
+    @RequestMapping("/3")
+    public String page3()
+    {
+        return "page3";
+    }
+}
